65514beb48bb568c06ab1f8c4f8f56bc878e5519
hawawaii <fnzkaps@naver.com>
2025-10-21 15:18:26 +0900

    ch9.2

--- vec3.h
+++ vec3.h
@@ -3,48 +3,56 @@
 #define VEC3_H
 
 class vec3 {
-public:
-	double e[3];
-	vec3() : e{ 0,0,0 } {}
-	vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
+	public:
+		double e[3];
+		vec3() : e{ 0,0,0 } {}
+		vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
 
-	double x() const { return e[0]; }
-	double y() const { return e[1]; }
-	double z() const { return e[2]; }
+		double x() const { return e[0]; }
+		double y() const { return e[1]; }
+		double z() const { return e[2]; }
 
-	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+		vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
 
-	//배열처럼 사용하기 위해
-	double operator[](int i) const { return e[i]; }	//const일때
-	double& operator[](int i) { return e[i]; }		//값을 변경할때
+		//배열처럼 사용하기 위해
+		double operator[](int i) const { return e[i]; }	//const일때
+		double& operator[](int i) { return e[i]; }		//값을 변경할때
 
-	//임시 객체 없이 직접 수정하기 위해
-	vec3& operator+=(const vec3& v) {
-		e[0] += v.e[0];
-		e[1] += v.e[1];
-		e[2] += v.e[2];
-		return *this;
-	}
+		//임시 객체 없이 직접 수정하기 위해
+		vec3& operator+=(const vec3& v) {
+			e[0] += v.e[0];
+			e[1] += v.e[1];
+			e[2] += v.e[2];
+			return *this;
+		}
 
-	//벡터에 스칼라값 곱함
-	vec3& operator*=(double t) {
-		e[0] *= t;
-		e[1] *= t;
-		e[2] *= t;
-		return *this;
-	}
+		//벡터에 스칼라값 곱함
+		vec3& operator*=(double t) {
+			e[0] *= t;
+			e[1] *= t;
+			e[2] *= t;
+			return *this;
+		}
 
-	vec3& operator/=(double t) {
-		return *this *= 1 / t;
-	}
+		vec3& operator/=(double t) {
+			return *this *= 1 / t;
+		}
 
-	double length() const {
-		return std::sqrt(length_squared());
-	}
+		double length() const {
+			return std::sqrt(length_squared());
+		}
 
-	double length_squared() const {
-		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
-	}
+		double length_squared() const {
+			return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+		}
+
+		static vec3 random() {
+			return vec3(random_double(), random_double(), random_double());
+		}
+
+		static vec3 random(double min, double max) {
+			return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+		}
 };
 
 //별칭. 명확성을 위해 point와 vector구분
@@ -98,6 +106,23 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 //단위벡터
 inline vec3 unit_vector(const vec3& v) {
 	return v / v.length();
+}
+
+inline vec3 random_unit_vector() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        auto lensq = p.length_squared();
+        if (1e-160 < lensq && lensq <= 1)
+            return p / sqrt(lensq);
+    }
+}
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
 }
 
 #endif
