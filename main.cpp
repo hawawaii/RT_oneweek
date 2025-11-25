61552331097f277d22406f78b1787a9794a61bed
hawawaii <fnzkaps@naver.com>
2025-11-25 13:33:22 +0900

    ch12 camera_position

--- main.cpp
+++ main.cpp
@@ -10,15 +10,31 @@
 int main() {
     hittable_list world;
 
-    auto material_lamb01 = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_lamb02 = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_diel01   = make_shared<dielectric>(1.50);
-    auto material_dielbub01 = make_shared<dielectric>(1.00 / 1.50);
-    auto material_metal01  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    //auto material_lamb01 = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    //auto material_lamb02 = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    //auto material_diel01   = make_shared<dielectric>(1.50);
+    //auto material_dielbub01 = make_shared<dielectric>(1.00 / 1.50);
+    //auto material_metal01  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     //world.add(make_shared<sphere>(point3( 0.0, 0.0, 0.0), 100.0, material_));
     //world.add(make_shared<quad>(point3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 0.0), material_));
+
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left   = make_shared<dielectric>(1.50);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
     
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
+    //world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    //world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
+
+    //world.add(make_shared<quad>(point3(-0.5, 0.5, 0.0), vec3(-1.0, 0.0, 0.0), vec3(0.0, -1.0, 0.0), material_metal01));
+    //world.add(make_shared<quad>(point3(-0.5, 0.5, 0.0), vec3(-1.0, 0.0, 0.0), vec3(0.0, 0.0, -1.0), material_metal01));
+    //world.add(make_shared<quad>(point3(-0.5, 0.5, 0.0), vec3(0.0, -1.0, 0.0), vec3(0.0, 0.0, -1.0), material_metal01));
+    
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     camera cam;
 
@@ -26,6 +42,11 @@ int main() {
     cam.image_width  = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
+
+    cam.vfov     = 50;
+    cam.lookfrom = point3(2,2,1);
+    cam.lookat   = point3(0,0,-1);
+    cam.vup      = vec3(0,1,0);
 
     cam.render(world);
 }
