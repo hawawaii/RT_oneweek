3f9871761dcb3e90da35859c063a874cfac1d121
hawawaii <fnzkaps@naver.com>
2025-12-02 17:35:03 +0900

    251202

--- main.cpp
+++ main.cpp
@@ -10,13 +10,21 @@
 int main() {
     hittable_list world;
 
-    auto material_lamb01 = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_lamb02 = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_diel01   = make_shared<dielectric>(1.50);
-    auto material_dielbub01 = make_shared<dielectric>(1.00 / 1.50);
-    auto material_metal01  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    // 난반사
+    //auto material_lamb_01 = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    //auto material_lamb_02 = make_shared<lambertian>(color(0.1, 0.2, 0.5));
 
+    // 유전체
+    //auto material_diel_01   = make_shared<dielectric>(1.50); 
+    //auto material_dielbub_01 = make_shared<dielectric>(1.00 / 1.50);
+
+    // 금속
+    //auto material_metal_01  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0); 
+
+    // 구체
     //world.add(make_shared<sphere>(point3( 0.0, 0.0, 0.0), 100.0, material_));
+
+    // 사각평면
     //world.add(make_shared<quad>(point3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 0.0), material_));
     
 
@@ -30,6 +38,7 @@ int main() {
     cam.vfov     = 50;
     cam.lookfrom = point3(2,2,1);
     cam.lookat   = point3(0,0,-1);
+    // 머리위
     cam.vup      = vec3(0,1,0);
 
     cam.render(world);
